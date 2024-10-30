c8379bbd4c749268f04f6279bea2f5af4990b69d
Srishti Sharma <147276642+srishtiisharmaa@users.noreply.github.com>
2024-10-30 00:30:34 +0000

    bootstrap skinning

--- app/content.tsx
+++ app/content.tsx
@@ -1,29 +1,49 @@
-"use client"
-
-import React from "react";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 import "bootstrap/dist/css/bootstrap.css";
 
-export default function Content() {
+const supabase = createClient("https://lldiaoztisdqsbsurcdl.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsZGlhb3p0aXNkcXNic3VyY2RsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMzgxNjMsImV4cCI6MjA0NTgxNDE2M30.kMgDhv_KHs7HBEB548wrmp_ASFoENGjJ79duuoTa-wo");
 
-    return <>
-        <div className="container py-5">
+function App() {
+  const [countries, setCountries] = useState([]);
 
-            <div className="row justify-content-center">
-                <div className="col-lg-6">
+  useEffect(() => {
+    getCountries();
+  }, []);
 
-                    <h1 className="text-center m-0 mb-3">Country List</h1>
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
 
-                    <ul className="list-group">
-                        <li className="list-group-item">🇨🇦 Canada</li>
-                        <li className="list-group-item">🇺🇸 United States</li>
-                        <li className="list-group-item">🇧🇷 Brazil</li>
-                    </ul>
+  return (
 
-                    <p className="text-center mt-2 text-muted">TODO: Connect to database</p>
+    <div className="container py-5">
 
-                </div>
-            </div>
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+
+          <h1 className="text-center m-0 mb-3">Country List</h1>
+
+          <ul className="list-group">
+
+            {countries.map((country) => (
+              <li key={country.name} className="list-group-item">
+                {country.emoji}
+                &nbsp;
+                {country.name}
+                </li>
+            ))}
+          </ul>
+
+          <p className="text-center mt-2 text-muted">TODO: Connect to database</p>
 
         </div>
-    </>
+      </div>
+
+    </div>
+
+  );
 }
+
+export default App;
